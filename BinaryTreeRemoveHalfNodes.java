ecfb5aebd53947537c86a97026aa1a5c315a1aea
Palash <maxzeroedge@gmail.com>
2022-02-12 12:59:34 +0530

    Binary Tree Remove Half Nodes added GFG version

--- BinaryTreeRemoveHalfNodes.java
+++ BinaryTreeRemoveHalfNodes.java
@@ -33,7 +33,7 @@ public class BinaryTreeRemoveHalfNodes {
         root.right.right.left = new Node<>(4);
 		inOrderTraversal(root);
 		System.out.println("\nRemoving half nodes");
-		removeHalfNodes(root);
+		removeHalfNodesGFG(root);
 		inOrderTraversal(root);
 	}
 
@@ -53,14 +53,31 @@ public class BinaryTreeRemoveHalfNodes {
 
 	public static void removeHalfNodes(Node root) {
 		if(root != null) {
-			inOrderTraversal(root); System.out.println("");
+			root.left = connectGrandChild(root, root.left);
+			root.right = connectGrandChild(root, root.right);
 			if(isLeafNode(root)) {
 				return;
 			}
-			root.left = connectGrandChild(root, root.left);
 			removeHalfNodes(root.left);
-			root.right = connectGrandChild(root, root.right);
 			removeHalfNodes(root.right);
 		}
+	}
+
+	public static Node removeHalfNodesGFG(Node root) {
+		if(root == null) {
+			return root;
+		}
+		root.left = removeHalfNodesGFG(root.left);
+		root.right = removeHalfNodesGFG(root.right);
+		if(root.left == null && root.right == null) {
+			return root;
+		}
+		if(root.left == null) {
+			return root.right;
+		}
+		if(root.right == null) {
+			return root.left;
+		}
+		return root;
 	}
 }
